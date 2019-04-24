54fbd4f484c6ccdbe7e259a00422db789f246178
weitao <sk11zwt@126.com>
2019-04-24 08:34:32 +0800

    ...

--- pages/list/article.js
+++ pages/list/article.js
@@ -18,50 +18,46 @@ Page({
       }
     ],
     programme: [
-      { url: '#',
+      {
+        url: '#',
         brief: '陕汽的未来何去何从？从陕汽重卡2019商务年会看未来发展新走向',
         img: '/images/list01.png',
         name: '56物流网 物流生态新物种',
         date: '12-22',
         icon: '/images/icon01.jpg'
-      }, { url: '#', 
-        brief: '陕汽的未来何去何从？从陕汽重卡2019商务年会看未来发展新走向', 
-        img: '/images/list02.jpg', 
-        name: '56物流网 物流生态新物种', 
+      }, {
+        url: '#',
+        brief: '陕汽的未来何去何从？从陕汽重卡2019商务年会看未来发展新走向',
+        img: '/images/list02.jpg',
+        name: '56物流网 物流生态新物种',
         date: '10-15'
-      }, { url: '#', 
-        brief: '陕汽的未来何去何从？从陕汽重卡2019商务年会看未来发展新走向', 
-        img: '/images/list03.png', 
-        name: '56物流网 物流生态新物种', 
+      }, {
+        url: '#',
+        brief: '陕汽的未来何去何从？从陕汽重卡2019商务年会看未来发展新走向',
+        img: '/images/list03.png',
+        name: '56物流网 物流生态新物种',
         date: '9-25'
       }
     ],
     texts: [
-      { text: '首页', url: '#'},
-      { text: '资讯', url: '../list/article'},
-      { text: '选车', url: '#'},
-      { text: '租车', url: '#'},
-      { text: '二手车', url: '#'},
-      { text: '发货', url: '#'},
-      { text: '司机加盟', url: '#'},
-      { text: '56研究院', url: '#'},
-      { text: '黄金产业链', url: '#'},
-      { text: '视频', url: '#'}
+      { text: '首页', url: '#' },
+      { text: '资讯', url: '#' },
+      { text: '选车', url: '#' },
+      { text: '租车', url: '#' },
+      { text: '二手车', url: '#' },
+      { text: '发货', url: '#' },
+      { text: '司机加盟', url: '#' },
+      { text: '56研究院', url: '#' },
+      { text: '黄金产业链', url: '#' },
+      { text: '视频', url: '#' }
     ],
-    indicatorDots: true,  //小点
-    autoplay: true,  //是否自动轮播
-    interval: 3000,  //间隔时间
-    duration: 1000,  //滑动时间
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
   //事件处理函数
   clickMe() {
     console.log(1)
     this.setData({ motto: 'Hello World' })
   },
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -73,14 +69,14 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true,
-          
+
         })
       }
     } else {
@@ -96,7 +92,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
